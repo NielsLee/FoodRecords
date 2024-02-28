cf2a8d7d3cda03f3ca0f050d0371adfa734e8b06
Niels_Lee@outlook.com <A990131a@>
2024-02-28 22:52:23 +0800

    引入了Glide库，现在预览框能够显示正常方向的图片了

--- app/src/main/java/lying/fengfeng/foodrecords/ui/components/insertionDialog/FoodPreview.kt
+++ app/src/main/java/lying/fengfeng/foodrecords/ui/components/insertionDialog/FoodPreview.kt
@@ -4,7 +4,9 @@ import android.Manifest
 import android.app.Activity
 import android.content.Context
 import android.content.pm.PackageManager
-import android.graphics.BitmapFactory
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
 import android.util.Size
 import androidx.camera.view.CameraController
 import androidx.compose.foundation.Image
@@ -15,27 +17,32 @@ import androidx.compose.material.icons.filled.CameraAlt
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color.Companion.Black
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.graphics.toArgb
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.ujizin.camposer.CameraPreview
 import com.ujizin.camposer.state.CamSelector
 import com.ujizin.camposer.state.CameraState
 import com.ujizin.camposer.state.ImageTargetSize
 import com.ujizin.camposer.state.rememberCamSelector
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
 import lying.fengfeng.foodrecords.repository.FoodInfoRepo
-import lying.fengfeng.foodrecords.utils.ImageUtil
 
 @Composable
 fun FoodPreview(
@@ -65,15 +72,22 @@ fun FoodPreview(
                 }
             } else {
                 val picturePath = FoodInfoRepo.getAbsolutePictureDir() + dialogViewModel.pictureUUID.value
-                val bitmap = BitmapFactory.decodeFile(picturePath)
-                val rotate = ImageUtil.getImageOrientation(picturePath)
+                var bitmap by remember { mutableStateOf(createBitmap()) }
                 val imageBitmap = bitmap.asImageBitmap()
                 val painter = BitmapPainter(imageBitmap)
+
+                LaunchedEffect(Unit) {
+                    CoroutineScope(Dispatchers.IO).launch {
+                        bitmap = Glide.with(context).asBitmap().load(picturePath)
+                            .diskCacheStrategy(DiskCacheStrategy.NONE).skipMemoryCache(true)
+                            .submit().get()
+                    }
+                }
 
                 Image(
                     painter = painter,
                     contentDescription = null,
-                    modifier = Modifier.fillMaxSize().rotate(rotate.toFloat())
+                    modifier = Modifier.fillMaxSize()
                 )
             }
         } else {
@@ -102,4 +116,16 @@ fun FoodPreview(
             }
         }
     }
+}
+
+fun createBitmap(): Bitmap {
+
+    val bitmap = Bitmap.createBitmap(300, 400, Bitmap.Config.ARGB_8888)
+    val canvas = Canvas(bitmap)
+    val paint = android.graphics.Paint().apply {
+        color = Color.argb(127, Black.toArgb(), Black.toArgb(), Black.toArgb())
+    }
+    canvas.drawRect(0f, 0f, 300f, 400f, paint)
+
+    return bitmap
 }
